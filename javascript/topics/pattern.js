fbcd73e847c15f6d34318f269c15e08ef2de2535
Alexandru Boncut <boncut.alex10@gmail.com>
2023-07-29 23:06:32 -0500

    Add new topic: Algo Patterns

---
+++ javascript/topics/pattern.js
@@ -0,0 +1,160 @@
+export default class Pattern {
+    dataList = [
+        {
+            pattern: "Sliding Window",
+            questions: [
+                {name: "Smallest Subarray with a given sum (easy)", checked: false},
+                {name: "Maximum Sum Subarray of Size K (easy)", checked: false},
+                {name: "Longest Substring with K Distinct Characters (medium)", checked: false},
+                {name: "Fruits into Baskets (medium)", checked: false},
+                {name: "No-repeat Substring (hard)", checked: false},
+                {name: "Longest Substring with Same Letters after Replacement (hard)", checked: false},
+            ],
+        },
+        {
+            pattern: "Two Pointers",
+            questions: [
+                {name: "Pair with Target Sum (easy)", checked: false},
+                {name: "Remove Duplicates (easy)", checked: false},
+                {name: "Squaring a Sorted Array (easy)", checked: false},
+                {name: "Triplet Sum to Zero (medium)", checked: false},
+                {name: "Triplets with Smaller Sum (medium)", checked: false},
+                {name: "Subarrays with Product Less than a Target (medium)", checked: false},
+                {name: "Dutch National Flag Problem (medium)", checked: false},
+            ]
+        },
+        {
+            pattern: "Fast & Slow Pointers",
+            questions: [
+                {name: "LinkedList Cycle (easy)", checked: false},
+                {name: "Start of LinkedList Cycle (medium)", checked: false},
+                {name: "Middle of the LinkedList (easy)", checked: false},
+                {name: "Happy Number (medium)", checked: false},
+            ],
+        },
+        {
+            pattern: "Cyclic Sort",
+            questions: [
+                {name: "Cyclic Sort (easy)", checked: false},
+                {name: "Find the Missing Number (easy)", checked: false},
+                {name: "Find all Missing Numbers (easy)", checked: false},
+                {name: "Find the Duplicate Number (easy)", checked: false},
+                {name: "Find all Duplicate Numbers (easy)", checked: false},
+                {name: "Find the Corrupt Pair (easy)", checked: false},
+            ],
+        },
+        {
+            pattern: "Merge Intervals",
+            questions: [
+                {name: "Merge Intervals (medium)", checked: false},
+                {name: "Insert Interval (medium)", checked: false},
+                {name: "Intervals Intersection (medium)", checked: false},
+                {name: "Conflicting Appointments (medium)", checked: false},
+                {name: "Minimum Meeting Rooms (hard)", checked: false},
+                ]
+        },
+        {
+            pattern: "Top K Elements",
+            questions: [
+                {name: "Top K Numbers (easy)", checked: false},
+                {name: "Kth Smallest Number (easy)", checked: false},
+                {name: "Kth Largest Number in a Stream (medium)", checked: false},
+                {name: "Connect Ropes (easy)", checked: false},
+                {name: "Top K Frequent Numbers (medium)", checked: false},
+                {name: "Kth Smallest Number in M Sorted Lists (medium)", checked: false},
+                {name: "Kth Smallest Number in a Sorted Matrix (hard)", checked: false},
+                ]
+        },
+        {
+            pattern: "In-place Reversal of a LinkedList",
+            questions: [
+                {name: "Reverse a LinkedList (easy)", checked: false},
+                {name: "Reverse a Sub-list (medium)", checked: false},
+                {name: "Reverse every K-element Sub-list (medium)", checked: false},
+            ]
+        },
+        {
+            pattern: "Tree Breadth First Search",
+            questions: [
+                {name: "Binary Tree Level Order Traversal (easy)", checked: false},
+                {name: "Reverse Level Order Traversal (easy)", checked: false},
+                {name: "Zigzag Traversal (medium)", checked: false},
+                {name: "Level Averages in a Binary Tree (easy)", checked: false},
+                {name: "Minimum Depth of a Binary Tree (easy)", checked: false},
+                {name: "Connect Level Order Siblings (medium)", checked: false},
+                {name: "Level Order Successor (easy)", checked: false},
+            ]
+        },
+        {
+            pattern: "Tree Depth First Search",
+            questions: [
+                {name: "Binary Tree Path Sum (easy)", checked: false},
+                {name: "All Paths for a Sum (medium)", checked: false},
+                {name: "Sum of Path Numbers (medium)", checked: false},
+                {name: "Path With Given Sequence (medium)", checked: false},
+                {name: "Count Paths for a Sum (medium)", checked: false},
+            ]
+        },
+        {
+            pattern: "Bitwise XOR",
+            questions: [
+                    {name: "Single Number (easy)", checked: false},
+                {name: "Two Single Numbers (medium)", checked: false},
+                {name: "Complement of Base 10 Number (medium)", checked: false},
+            ]
+        },
+        {
+            pattern: "Subsets",
+            questions: [
+                    {name: "Subsets (easy)", checked: false},
+                {name: "Subsets With Duplicates (easy)", checked: false},
+                {name: "Permutations (medium)", checked: false},
+                {name: "String Permutations by changing case (medium)", checked: false},
+                {name: "Balanced Parentheses (hard)", checked: false},
+            ]
+        },
+        {
+            pattern: "Modified Binary Search",
+            questions: [
+                    {name: "Order-agnostic Binary Search (easy)", checked: false},
+                {name: "Ceiling of a Number (medium)", checked: false},
+                {name: "Next Letter (medium)", checked: false},
+                {name: "Number Range (medium)", checked: false},
+                {name: "Search in a Sorted Infinite Array (medium)", checked: false},
+                {name: "Minimum Difference Element (medium)", checked: false},
+            ]
+        },
+        {
+            pattern: "Topological Sort (Graph",
+            questions: [
+                {name: "Topological Sort (medium)", checked: false},
+                {name: "Tasks Scheduling (medium)", checked: false},
+                {name: "Tasks Scheduling Order (medium)", checked: false},
+                {name: "All Tasks Scheduling Orders (hard)", checked: false},
+                {name: "Alien Dictionary (hard)", checked: false},
+            ]
+        },
+        {
+            pattern: "K-way merge",
+            questions: [
+                {name: "Merge K Sorted Lists (medium)", checked: false},
+                {name: "Kth Smallest Number in a Sorted Matrix (Hard)", checked: false},
+                {name: "Smallest Number Range (Hard)", checked: false},
+            ]
+        },
+        {
+            pattern: "0 or 1 Knapsack (Dynamic Programming",
+            questions: [
+                {name: "Subset Sum (medium)", checked: false},
+                {name: "Equal Subset Sum Partition (medium)", checked: false},
+                {name: "Minimum Subset Sum Difference (hard)", checked: false},
+            ]
+        },
+        {
+            pattern: "Miscellaneous",
+            questions: [
+                {name: "Kth Smallest Number (hard)", checked: false},
+            ]
+        }
+    ]
+}
